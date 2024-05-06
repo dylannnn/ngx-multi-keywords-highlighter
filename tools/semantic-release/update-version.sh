df90a189154b4a2de3ac59ed10160e47caec38f6
Yunfei Li <dylannnnlee@gmail.com>
2024-05-06 12:01:56 +0200

    fix: resolve package.json version

--- tools/semantic-release/update-version.sh
+++ tools/semantic-release/update-version.sh
@@ -9,9 +9,11 @@ sed -i '' "s/$LAST_RELEASE_VERSION/$NEXT_RELEASE_VERSION/g" libs/ngx-multi-keywo
 # Update package.json and package-lock.json version
 PACKAGE_JSON_FILE="package.json"
 PACKAGE_LOCK_JSON_FILE="package-lock.json"
+LIBRARY_PACKAGE_JSON_FILE="libs/ngx-multi-keywords-highlighter/package.json"
 
 jq --arg NEXT_RELEASE_VERSION "$NEXT_RELEASE_VERSION" '.version = $NEXT_RELEASE_VERSION' "$PACKAGE_JSON_FILE" > "tmp-package.json" && mv "tmp-package.json" "$PACKAGE_JSON_FILE"
 jq --arg NEXT_RELEASE_VERSION "$NEXT_RELEASE_VERSION" '.packages."".version = $NEXT_RELEASE_VERSION | .version = $NEXT_RELEASE_VERSION' "$PACKAGE_LOCK_JSON_FILE" > "tmp-package-lock.json" && mv "tmp-package-lock.json" "$PACKAGE_LOCK_JSON_FILE"
+jq --arg NEXT_RELEASE_VERSION "$NEXT_RELEASE_VERSION" '.version = $NEXT_RELEASE_VERSION' "$LIBRARY_PACKAGE_JSON_FILE" > "tmp-lib-package.json" && mv "tmp-lib-package.json" "$LIBRARY_PACKAGE_JSON_FILE"
 
 # Update compodoc version
 sed -i '' "s/$LAST_RELEASE_VERSION/$NEXT_RELEASE_VERSION/g" .compodocrc.yaml
